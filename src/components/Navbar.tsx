e709c5ddaddcc7d0528690c1a4ebc666b63076a9
Abdul Rehman <abdulrehmantahir142@gmail.com>
2025-08-01 11:43:16 +0500

    feat:navalign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,25 +5,18 @@ function Navbar() {
     <div className="bg-[#280cb3] text-white border-b border-gray-950">
       <div className="flex justify-between items-center px-4 py-3">
         <div className="text-lg font-bold">My App</div>
-        
+
+        {/* Desktop Nav */}
         <ul className="hidden sm:flex space-x-6 text-base font-medium">
-          <li>
-            <a href="/" className="hover:text-amber-400">Home</a>
-          </li>
-          <li>
-            <a href="/ToDo" className="hover:text-amber-400">To-Do</a>
-          </li>
-          <li>
-            <a href="/Contact" className="hover:text-amber-400">Contact</a>
-          </li>
-          <li>
-            <a href="/About" className="hover:text-amber-400">About</a>
-          </li>
+          <li><a href="/" className="hover:text-amber-400">Home</a></li>
+          <li><a href="/ToDo" className="hover:text-amber-400">To-Do</a></li>
+          <li><a href="/Contact" className="hover:text-amber-400">Contact</a></li>
+          <li><a href="/About" className="hover:text-amber-400">About</a></li>
         </ul>
       </div>
 
-    
-      <div className="sm:hidden text-center text-sm py-2 space-y-1">
+      {/* Mobile Nav */}
+      <div className="sm:hidden text-center text-sm pb-3 space-y-1">
         <a href="/" className="block hover:text-amber-400">Home</a>
         <a href="/ToDo" className="block hover:text-amber-400">To-Do</a>
         <a href="/Contact" className="block hover:text-amber-400">Contact</a>
